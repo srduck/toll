683a192c314f0a432ad30aff6486ce6bc1c6ab31
srduck-local <srdkr@yandex.ru>
2017-07-07 01:23:09 +0700

    ++ junit, jackson-databind, PointDTO. Преобразование объекта в json

---
+++ common/src/main/java/srduck/tdo/PointDTO.java
@@ -0,0 +1,51 @@
+package srduck.tdo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Created by igor on 07.07.2017.
+ */
+public class PointDTO {
+    private double lat;
+    private double lon;
+    private String autoId;
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public String getAutoId() {
+        return autoId;
+    }
+
+    public void setAutoId(String autoId) {
+        this.autoId = autoId;
+    }
+
+    public String toJson() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(this);
+    }
+
+    @Override
+    public String toString() {
+        return "PointDTO{" +
+                "lat=" + lat +
+                ", lon=" + lon +
+                ", autoId='" + autoId + '\'' +
+                '}';
+    }
+}
---
+++ common/src/test/java/srduck/tdo/PointDTOTest.java
@@ -0,0 +1,22 @@
+package srduck.tdo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by igor on 07.07.2017.
+ */
+public class PointDTOTest {
+
+    @Test
+    public void toJson() throws Exception{
+        PointDTO point = new PointDTO();
+        point.setLat(56);
+        point.setLon(74);
+        point.setAutoId("o567gfd");
+        assertTrue(point.toJson().contains("\"lat\":56"));
+        System.out.println(point.toJson());
+    }
+
+}
