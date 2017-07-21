795c200da5f22e700ba57359289e9820e7de6e2b
srduck-local <srdkr@yandex.ru>
2017-07-22 00:00:10 +0700

    Конфигурация контекста (GPSContext), создание файла app.properties

--- tracker-core/src/main/java/srduck/GPSContext.java
+++ tracker-core/src/main/java/srduck/GPSContext.java
@@ -1,12 +1,49 @@
 package srduck;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import srduck.services.GPSService;
+import srduck.services.GPSToolService;
+import srduck.services.MessageSendService;
+import srduck.services.MessageStorageService;
 
 /**
  * Created by igor on 21.07.2017.
  */
 @Configuration
 @EnableScheduling
+@PropertySource("classpath:/app.properties")
 public class GPSContext {
+
+    @Bean
+    GPSService gpsService(){
+        return new GPSService();
+    }
+
+    @Bean
+    GPSToolService gpsToolService(){
+        return new GPSToolService();
+    }
+
+    @Bean
+    MessageSendService messageSendService(){
+        return new MessageSendService();
+    }
+
+    @Bean
+    MessageStorageService messageStorageService(){
+        return new MessageStorageService();
+    }
+
+    @Bean
+    public TaskScheduler poolSheduler(){
+        ThreadPoolTaskScheduler sheduler = new ThreadPoolTaskScheduler();
+        sheduler.setThreadNamePrefix("poolSheduler");
+        sheduler.setPoolSize(20);
+        return sheduler;
+    }
 }
