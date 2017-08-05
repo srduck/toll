f109f15a7b762dc6c157d7f8856dc29bf533bd83
srduck-local <srdkr@yandex.ru>
2017-08-05 19:18:48 +0700

    Добавление возможности записи координат в файл в server-core -> TrackerController

--- server-core/src/main/java/srduck/controllers/TrackerController.java
+++ server-core/src/main/java/srduck/controllers/TrackerController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 import srduck.dto.Coordinates;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
-import java.io.IOException;
+import javax.annotation.PreDestroy;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 /**
  * Created by igor on 05.08.2017.
@@ -21,17 +22,38 @@ public class TrackerController {
 
     private static Logger log = LoggerFactory.getLogger(TrackerController.class);
 
+    private PrintWriter printWriter;
+
+    @PostConstruct
+    private void init(){
+
+        try {
+            printWriter = new PrintWriter("coordinates.txt");
+        }
+        catch(FileNotFoundException nfe) {
+            nfe.getMessage();
+        }
+
+    }
+
     @RequestMapping(value = "/coords", method = RequestMethod.POST, consumes = "application/json")
     public Boolean getCoordinates (@RequestBody String jsonCoordinates){
         try {
             ObjectMapper mapper = new ObjectMapper();
             Coordinates coordinates = mapper.readValue(jsonCoordinates, Coordinates.class);
             log.info("getCoordinates: lat = " + coordinates.getLat() + "; lon = " + coordinates.getLon());
+            printWriter.println(coordinates.getLon() + "," + coordinates.getLat());
+            printWriter.flush();
             return true;
         }
         catch (Exception e){
             e.getStackTrace();
             return false;
         }
+    }
+
+    @PreDestroy
+    private void distroy(){
+        printWriter.close();
     }
 }
--- tracker-core/src/main/java/srduck/GPSMain.java
+++ tracker-core/src/main/java/srduck/GPSMain.java
@@ -3,8 +3,6 @@ package srduck;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
