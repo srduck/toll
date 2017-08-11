1825da289feb28abbe75d85cb1786b9fe6770b01
srduck-local <srdkr@yandex.ru>
2017-08-12 02:47:39 +0700

    ++ код файлов конфигурации, css. ++ код в templateв: error, home, login. Html-файлы: register(Client/Manager), payments, routes без кода в body

--- server-ui/src/main/java/srduck/config/MvcConfig.java
+++ server-ui/src/main/java/srduck/config/MvcConfig.java
@@ -1,7 +1,27 @@
 package srduck.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 /**
  * Created by igor on 12.08.2017.
  */
-public class MvcConfig {
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry){
+
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/error").setViewName("error");
+        registry.addViewController("/routes").setViewName("routes");
+        registry.addViewController("/payments").setViewName("payments");
+        registry.addViewController("/registerClient").setViewName("registerClient");
+        registry.addViewController("/registerManager").setViewName("registerManager");
+
+    }
+
 }
--- server-ui/src/main/java/srduck/config/SecurityConfig.java
+++ server-ui/src/main/java/srduck/config/SecurityConfig.java
@@ -1,7 +1,44 @@
 package srduck.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 /**
  * Created by igor on 12.08.2017.
  */
-public class SecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure (HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**","/img/**","/error").permitAll()
+                .antMatchers("/payments*","/routes*").hasRole("CLIENT")
+                .antMatchers("/registerClient*").hasRole("MANAGER")
+                .antMatchers("/registerManager*").hasRole("ROOT")
+                .antMatchers("/home").authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll().defaultSuccessUrl("/home", true)
+                .and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception{
+        auth
+                .inMemoryAuthentication()
+                .withUser("guest").password("").roles("")
+                .and()
+                .withUser("client").password("client").roles("CLIENT")
+                .and()
+                .withUser("manager").password("manager").roles("CLIENT","MANAGER")
+                .and()
+                .withUser("root").password("root").roles("CLIENT","MANAGER","ROOT");
+    }
 }
